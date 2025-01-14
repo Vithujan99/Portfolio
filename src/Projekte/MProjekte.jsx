c5ea09d73ea70c4c659eb54237dee015c3fce9e7
Vithujan99 <vithujan99@hotmail.com>
2025-01-14 13:08:32 +0100

    Projekte Section for Mobile

--- src/Projekte/MProjekte.jsx
+++ src/Projekte/MProjekte.jsx
@@ -22,17 +22,25 @@ const MProjekte = () => {
 
   useEffect(() => {
     // Animate project holders as they scroll into view
-    gsap.from(".M-projekt-holder", {
-      opacity: 0,
-      y: 50,
-      duration: 1,
-      ease: "power2.out",
-      scrollTrigger: {
-        trigger: ".M-projekte-holder",
-        start: "top 80%", // Start animation when the top of the container is 80% of the viewport
-        end: "bottom 20%", // End animation when the bottom of the container is 20% of the viewport
-        scrub: true, // Smooth animation as the user scrolls
-      },
+    const projekt = document.querySelectorAll(".M-projekt-holder");
+    projekt.forEach((projekt) => {
+      gsap.fromTo(
+        projekt,
+        { opacity: 0, y: 50 }, // Initial state
+        {
+          opacity: 1,
+          y: 0,
+          duration: 1,
+          ease: "power2.out",
+          scrollTrigger: {
+            trigger: projekt,
+            start: "top 80%", // Start animation when the top of the container is 80% of the viewport
+            end: "top 40%", // End animation when the bottom of the container is 20% of the viewport
+            scrub: true, // Smooth animation as the user scrolls
+            toggleActions: "play reverse play reverse", // Play and reverse animations
+          },
+        }
+      );
     });
 
     // Play video when it enters the center of the screen
@@ -40,8 +48,8 @@ const MProjekte = () => {
     videos.forEach((video) => {
       ScrollTrigger.create({
         trigger: video,
-        start: "top 50%", // Trigger when the top of the video is at 50% of the viewport
-        end: "bottom 50%", // Trigger until the bottom of the video leaves 50% of the viewport
+        start: "top 70%", // Trigger when the top of the video is at 70% of the viewport
+        end: "bottom 30%", // Trigger until the bottom of the video leaves 30% of the viewport
         onEnter: () => video.play(),
         onLeave: () => video.pause(),
         onEnterBack: () => video.play(),
@@ -102,6 +110,51 @@ const MProjekte = () => {
                 muted
                 autoPlay
                 poster="thumbnails/hero-2.png"
+                id="next-video"
+                onWaiting={handleWaiting}
+                onCanPlay={handleCanPlay}
+                onLoadedData={handleLoadedData}
+              />
+              {isLoading && <div className="M-spinner"></div>}
+            </div>
+          </div>
+
+          <div className="M-projekt-holder">
+            <a className="M-projekt-info-holder">
+              <p className="M-projekt-name">Rustica</p>
+              <div className="M-projekt-tools-holder">
+                <p>React</p>
+                <p>MongoDB</p>
+                <p>Node.js</p>
+              </div>
+            </a>
+            <div className="M-video-container">
+              <video
+                className="M-projekt-video"
+                src="videos/hero-3.mp4"
+                loop
+                muted
+                poster="thumbnails/hero-3.png"
+                id="next-video"
+                onWaiting={handleWaiting}
+                onCanPlay={handleCanPlay}
+                onLoadedData={handleLoadedData}
+              />
+              {isLoading && <div className="M-spinner"></div>}
+            </div>
+          </div>
+
+          <div className="M-projekt-holder">
+            <a className="M-projekt-info-holder">
+              <p className="M-projekt-name">Webflow</p>
+            </a>
+            <div className="M-video-container">
+              <video
+                className="M-projekt-video"
+                src="videos/hero-4.mp4"
+                loop
+                muted
+                poster="thumbnails/hero-4.png"
                 id="next-video"
                 onWaiting={handleWaiting}
                 onCanPlay={handleCanPlay}
